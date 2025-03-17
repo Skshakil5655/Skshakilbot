8c71b28126d01a8f2b0b03155a53bfe1bc9283a3
Skshakil5655 <sksk11xx@gmail.com>
2025-03-18 02:12:14 +0600

    Update Shakil.js

--- Shakil.js
+++ Shakil.js
@@ -1,1 +1,12 @@
-
+module.exports = {
+  bot: {
+    name: "MyFacebookBot",
+    ownerID: "YOUR_FACEBOOK_ID",
+    prefix: "!",
+    autoRestart: true
+  },
+  fbConfig: {
+    useFbState: true,
+    fbStatePath: "./Shakilstate.json"
+  }
+};
